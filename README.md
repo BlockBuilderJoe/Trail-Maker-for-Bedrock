af566399f472460af64731ed76f988367e94080d
Joe <joe@blockbuilders.co.uk>
2023-07-19 10:02:19 +0100

    Prototype version of the Trail Maker 3000
    To improve in the future:
    
    - Add encrypted websocket option.
    - Remove the need for makecode.
    - Make the trail be placed as you go.
    - Give the player the ability to edit the trail if they make a mistake.

---
+++ README.md
@@ -0,0 +1,22 @@
+
+Install the dependencies for this script by navigating to this folder in a terminal and typing. 
+
+``` 
+pip install -r requirements.txt
+```
+
+
+For this to work you need to import this Makecode workspace. https://makecode.com/_VrR3Ke5VmLiu
+
+How to use:
+
+1. Remove encrypted ws option from main menu. 
+2. Download the workspace
+3. Activate the workspace
+4. Run trail.py
+5. type /connect localhost:3000
+6. press t then x
+7. check the function output
+8. To stop the script running click CTRL + C in the terminal. 
+
+
---
+++ trail.py
@@ -0,0 +1,64 @@
+import asyncio
+import websockets
+import json
+from uuid import uuid4
+import re
+
+######################################### Variables ############################################################################
+
+
+#What summon command do you want to prefix to the coordinate?
+prefix_command = "summon blockbuilders:trail"
+#Where do you want to write the mcfunction to?
+output_file = "/Users/joe/Library/Application Support/minecraftpe/games/com.mojang/development_behavior_packs/BP/functions/trail.mcfunction"
+
+####################################################################################################################################
+async def blockbuilders_ws(websocket):
+    print('Connected')
+    # Subscribe to the PlayerMessage event
+    await websocket.send(
+        # Send the subscribe message
+        json.dumps({
+            "header": {
+                "version": 1,
+                "requestId": f'{uuid4()}',
+                # Subscribe to the PlayerMessage event
+                "messageType": "commandRequest",
+                "messagePurpose": "subscribe"
+            },
+            "body": {
+                "eventName": "PlayerMessage"
+            },
+        }))
+
+    try:
+        async for msg in websocket:
+            # Load the message
+            msg = json.loads(msg)
+            # Print the message
+            print(msg)
+            # Check if the message is a chat message
+            if 'message' in msg['body']:
+                # Get the message
+                message = msg['body']['message']
+                # Check if the message starts with [
+                if message.startswith('['):
+                    # use regex to remove everything between [ and ]
+                    message = re.sub(r'\[.*?\]', '', message)
+                    print(message)
+                    # Writes the command to a functions file.
+                    with open(output_file, 'a') as f:
+                        f.write(f"{prefix_command}{message}" + '\n')
+
+            
+    except websockets.exceptions.ConnectionClosedError:
+        print('Disconnected from MineCraft')
+
+
+async def main():
+    async with websockets.serve(blockbuilders_ws, host='localhost', port=3000):
+        print('command to connect: /connect localhost:3000')
+        await asyncio.Future()
+
+
+asyncio.run(main())
